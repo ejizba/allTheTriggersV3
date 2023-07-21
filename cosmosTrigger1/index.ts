cfe3e189607c694b8177ece1219c3d8f5b85d96f
Eric Jizba <erijiz@microsoft.com>
2023-07-21 11:46:16 -0700

    remove other triggers

--- cosmosTrigger1/index.ts
+++
@@ -1,7 +0,0 @@
-import { AzureFunction, Context } from "@azure/functions"
-
-const cosmosDBTrigger: AzureFunction = async function (context: Context, documents: any[]): Promise<void> {
-    context.log(`Cosmos DB function processed ${documents.length} documents`);
-}
-
-export default cosmosDBTrigger;
