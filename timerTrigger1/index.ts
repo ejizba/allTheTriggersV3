7dd14703737e3cd2af40de60dae87b9f9242e408
Eric Jizba <erijiz@microsoft.com>
2023-07-21 11:45:11 -0700

    remove other triggers

--- timerTrigger1/index.ts
+++
@@ -1,7 +0,0 @@
-import { AzureFunction, Context } from "@azure/functions"
-
-const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
-    context.log('Timer function processed request.');
-};
-
-export default timerTrigger;
