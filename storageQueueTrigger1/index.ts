077f9e9b8e480101e6ee14b58ddc4df1406eed1a
Eric Jizba <erijiz@microsoft.com>
2023-07-24 16:18:44 -0700

    with output

--- storageQueueTrigger1/index.ts
+++ storageQueueTrigger1/index.ts
@@ -2,6 +2,7 @@ import { AzureFunction, Context } from "@azure/functions"
 
 const queueTrigger: AzureFunction = async function (context: Context, queueItem: string): Promise<void> {
     context.log('Storage queue function processed work item:', queueItem);
+    context.bindings.outputQueueItem = queueItem;
 };
 
 export default queueTrigger;
