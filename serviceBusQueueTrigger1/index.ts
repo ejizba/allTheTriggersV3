e44fc358167db9ff47cfef1c900588780ca34c07
Eric Jizba <erijiz@microsoft.com>
2023-07-31 20:52:47 -0700

    queue in and out

--- serviceBusQueueTrigger1/index.ts
+++ serviceBusQueueTrigger1/index.ts
@@ -2,6 +2,7 @@ import { AzureFunction, Context } from "@azure/functions"
 
 const serviceBusQueueTrigger: AzureFunction = async function(context: Context, message: any): Promise<void> {
     context.log('Service bus queue function processed message:', message);
+    context.bindings.sboutput = message;
 };
 
 export default serviceBusQueueTrigger;
